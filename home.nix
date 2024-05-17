68f12917cad403f9e2ed14bd9ee30dd85e994f9b
bivsk <bivsk@tutanota.com>
2024-05-17 00:30:53 -0400

    Base home-manager config

--- home.nix
+++ home.nix
@@ -19,21 +19,10 @@
     p7zip
 
     # utils
-    ripgrep # recursively searches directories for a regex pattern
-    jq # A lightweight and flexible command-line JSON processor
-    yq-go # yaml processor https://github.com/mikefarah/yq
-    eza # A modern replacement for ‘ls’
-    fzf # A command-line fuzzy finder
-
-    # networking tools
-    mtr # A network diagnostic tool
-    iperf3
-    dnsutils  # `dig` + `nslookup`
-    ldns # replacement of `dig`, it provide the command `drill`
-    aria2 # A lightweight multi-protocol & multi-source command-line download utility
-    socat # replacement of openbsd-netcat
-    nmap # A utility for network discovery and security auditing
-    ipcalc  # it is a calculator for the IPv4/v6 addresses
+    ripgrep
+    jq
+    eza
+    fzf
 
     # misc
     cowsay
@@ -46,25 +35,12 @@
     zstd
     gnupg
 
-    # nix related
-    #
-    # it provides the command `nom` works just like `nix`
-    # with more details log output
     nix-output-monitor
-
-    # productivity
-    hugo # static site generator
-    glow # markdown previewer in terminal
 
     btop  # replacement of htop/nmon
     iotop # io monitoring
     iftop # network monitoring
     nvtop
-
-    # system call monitoring
-    strace # system call monitoring
-    ltrace # library call monitoring
-    lsof # list open files
 
     # system tools
     sysstat
@@ -200,6 +176,8 @@
       selection.save_to_clipboard = true;
     };
   };
+
+  programs.mpv.enable = true;
 
   programs.bash = {
     enable = true;

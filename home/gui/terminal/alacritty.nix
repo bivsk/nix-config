d663b1953ef5904d9317c894882b5570dd302449
bivsk <bivsk@tutanota.com>
2024-05-20 17:55:00 -0400

    base alacritty config

---
+++ home/gui/terminal/alacritty.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+{
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      env.TERM = "xterm-256color";
+      font = {
+        size = 12;
+	draw_bold_text_with_bright_colors = true;
+      };
+      scrolling.multiplier = 5;
+      selection.save_to_clipboard = true;
+      window = {
+        opacity = 1.0;
+	padding = {
+	  x = 8;
+	  y = 8;
+	};
+      };
+    };
+  };
+}
--- home/home.nix
+++ home/home.nix
@@ -5,6 +5,7 @@
 }: {
   imports = [
     ./easyeffects.nix
+    ./gui
   ];
 
   nixpkgs = {
@@ -175,19 +176,6 @@
   };
 
   programs.firefox.enable = true;
-
-  programs.alacritty = {
-    enable = true;
-    settings = {
-      env.TERM = "xterm-256color";
-      font = {
-        size = 12;
-        draw_bold_text_with_bright_colors = true;
-      };
-      scrolling.multiplier = 5;
-      selection.save_to_clipboard = true;
-    };
-  };
 
   programs.mpv.enable = true;
 
